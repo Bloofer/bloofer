dbf370a71fd3dea00450aa9d64224c42d8f146c1
Yang <j0238@naver.com>
2017-07-20 16:56:18 +0900

    add board page

--- routes.py
+++ routes.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
   return render_template('home.html')
 
+@app.route('/board')
+def board():
+  return render_template('board.html')
+
 if __name__ == '__main__':
   app.run(debug=True)
